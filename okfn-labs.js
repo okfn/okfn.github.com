09e14c4a4c6292570d55741ebbfcdbfc30af83cb
Sam Smith <sam@mintcanary.com>
2012-03-20 11:54:47 +0000

    new project list

--- okfn-labs.js
+++ okfn-labs.js
@@ -5,3 +5,12 @@ $(function() {
     $('a[data-toggle="tab"][href=\''+tabName+'\']').tab('show')
   }
 });
+
+
+// Add ... after text
+$(document).ready(function() {
+		$(".project-list li .text").dotdotdot({
+				after: "a.read-more", // put after the ellipsis
+				watch: true, // allow it to work with tabs
+	});
+});
