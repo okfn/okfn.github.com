4fed0b1d47e9ee80a464aa69add5c356c94e1a79
Tom Rees <zephod@gmail.com>
2012-05-15 19:15:02 +0100

    Finished refactoring. It's a beauty.

--- okfn-labs.js
+++
@@ -1,8 +0,0 @@
-
-// Add ... after text
-$(document).ready(function() {
-		$(".project-table td .text").dotdotdot({
-				after: "a.read-more", // put after the ellipsis
-				watch: true, // allow it to work with tabs
-	});
-});
