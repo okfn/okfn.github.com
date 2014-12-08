918d48d241b3bc62bc7953bb0c23b4ef177f2ebf
Mike Chelen <michael.chelen@gmail.com>
2014-12-08 06:02:55 -0500

    include all project image tasks in grunt dist, remove intermediate file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,24 @@
 var fs   = require('fs');
 var yaml = require('js-yaml');
 var yamlFront = require('yaml-front-matter');
-var projectimages = require('./img/projectimages.json');
-
 
 module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    curl: projectimages,
+    curl: {},
     imagemin: {
       options: {
         optimizationLevel: 7,
         progressive: true
       },
-      projects: {                         // Another target
-      files: [{
-        expand: true,                  // Enable dynamic expansion
-        cwd: 'img/projects',                   // Src matches are relative to this path
-        src: ['*'],   // Actual patterns to match
-        dest: 'img/projects'                  // Destination path prefix
-      }]
+      projects: {
+        files: [{
+          expand: true,                  // Enable dynamic expansion
+          cwd: 'img/projects',                   // Src matches are relative to this path
+          src: ['*'],   // Actual patterns to match
+          dest: 'img/projects'                  // Destination path prefix
+        }]
       }
     }
   });
@@ -32,15 +30,17 @@ module.exports = function(grunt) {
 
   // Default task(s).
   grunt.registerTask('default', []);
+    
+  grunt.registerTask('dist', 'prepare site for distribution', function() {
+    // run all the tasks in order  
+    grunt.task.run(['projectimg', 'curl', 'imagemin']);
+  });
   
   grunt.registerTask('projectimg', 'generate json list of project images', function(arg) {
     // reads yaml frontmatter from each jekyll post in the projects folder
-    // saves output as img/projectimages.json
-
     var dirpath = "projects/_posts";
     var filenames = fs.readdirSync(dirpath);
     var imageurls = {};
-
     filenames.map(function(filename){
       // read frontmatter
       var fileyaml = yamlFront.loadFront(fs.readFileSync(dirpath + "/" + filename, 'utf8')); 
@@ -52,8 +52,7 @@ module.exports = function(grunt) {
         }
       }
     });
-
-    fs.writeFileSync("img/projectimages.json", JSON.stringify(imageurls)); //write to file
+    grunt.config('curl', imageurls);
   });
-
+  
 };
